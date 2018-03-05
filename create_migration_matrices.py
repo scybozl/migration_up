83e290579bfca53d5a86f2feaacbd910e74eac09
Ludovic Michel Scyboz <lscyboz@lxplus031.cern.ch>
2018-03-05 13:27:41 +0100

    corrected a few other bugs, changed plotting, bin ranges

--- create_migration_matrices.py
+++ create_migration_matrices.py
@@ -76,7 +76,6 @@ def ratioPlot(firstHist,secondHist,title):
   hist1.GetYaxis().SetTitleSize(20)
   hist1.GetYaxis().SetTitleFont(43)
   hist1.GetYaxis().SetTitleOffset(1.55)
-  hist1.GetYaxis().SetMinimum(0)
   hist1.GetXaxis().SetTitleSize(0)
   hist1.GetXaxis().SetLabelSize(0)
 
@@ -104,7 +103,9 @@ def ratioPlot(firstHist,secondHist,title):
   c2.Close()
 
 def populateHistsUp(reco, obsR, part, obsP):
+  os.system("mkdir " + outputHists)
   global recoOnly
+  recoFile = TFile(outputHists + "/reco_level.root", "NEW")
   for i in range(reco.GetEntries()):
     if ((i+1) % 1000) == 0: print "Processing event number " + str(i+1) + "..."
     reco.GetEntry(i)
@@ -132,9 +133,16 @@ def populateHistsUp(reco, obsR, part, obsP):
 	  if globals()[y].size() > 0: faccHists[shift+j].Fill(globals()[y][0])
 	  if globals()[x].size() > 0 and globals()[y].size() > 0:
 	    migrationMatrices[shift+j].Fill(globals()[x][0],globals()[y][0])
+  for j,obs in enumerate(observables):
+    recoHists[j].Write()
+  for j,vec in enumerate(vectors):
+    recoHists[shift+j].Write()
+  recoFile.Close()
 
 def populateHistsDown(reco, obsR, part, obsP):
+  os.system("mkdir " + outputHists)
   global partOnly
+  partFile = TFile(outputHists + "/part_level.root", "NEW")
   for i in range(part.GetEntries()):
     if ((i+1) % 1000) == 0: print "Processing event number " + str(i+1) + "..."
     part.GetEntry(i)
@@ -145,7 +153,11 @@ def populateHistsDown(reco, obsR, part, obsP):
         x = vec[1] + "_part"
         if globals()[x].size() > 0: partHists[shift+j].Fill(globals()[x][0])
     if reco.GetEntryWithIndex(obsP.runNumber, obsP.eventNumber) < 0: partOnly += 1
-
+  for j,obs in enumerate(observables):
+    partHists[j].Write()
+  for j,vec in enumerate(vectors):
+    partHists[shift+j].Write()
+  partFile.Close()
 
 def normalize():
   for i,obs in enumerate(observables):
@@ -199,16 +211,16 @@ def efficiencies():
   for i,obs in enumerate(observables):
     effHists[i].Divide(partHists[i])
     effHists[i].GetXaxis().SetTitle(obs[5])
-    effHists[i].GetYaxis().SetTitle("\epsilon_{eff} " + obs[5])
-    effHists[i].GetYaxis().SetMinimum(0)
+    effHists[i].GetYaxis().SetTitle("#epsilon_{eff} " + obs[5])
+    effHists[i].GetYaxis().SetRangeUser(0.,1.)
     effHists[i].Draw("*H")
     c.SaveAs(outputEff + "/efficiency_" + obs[1] + ".pdf")
     effHists[i].Write()
   for i,vec in enumerate(vectors):
     effHists[shift+i].Divide(partHists[shift+i])
     effHists[shift+i].GetXaxis().SetTitle(vec[5])
-    effHists[shift+i].GetYaxis().SetTitle("\epsilon_{eff} lead. " + vec[5])
-    effHists[shift+i].GetYaxis().SetMinimum(0)
+    effHists[shift+i].GetYaxis().SetTitle("#epsilon_{eff} lead. " + vec[5])
+    effHists[shift+i].GetYaxis().SetRangeUser(0.,1.)
     effHists[shift+i].Draw("*H")
     c.SaveAs(outputEff + "/efficiency_" + vec[1] + "1.pdf")
     effHists[shift+i].Write()
@@ -217,22 +229,26 @@ def efficiencies():
 
 def fakeHits():
   os.system("mkdir " + outputFacc)
+  fakeFile = TFile(outputFacc + "/fake_hits_rates.root", "NEW")
   c = TCanvas("c","c",900,900)
   c.cd()
   for i,obs in enumerate(observables):
     faccHists[i].Divide(recoHists[i])
     faccHists[i].GetXaxis().SetTitle(obs[5])
     faccHists[i].GetYaxis().SetTitle("f_{acc} " + obs[5])
-    faccHists[i].GetYaxis().SetMinimum(0)
+    faccHists[i].GetYaxis().SetRangeUser(0.,1.)
     faccHists[i].Draw("*H")
     c.SaveAs(outputFacc + "/fake_hits_rate_" + obs[1] + ".pdf")
+    faccHists[i].Write()
   for i,vec in enumerate(vectors):
     faccHists[shift+i].Divide(recoHists[shift+i])
     faccHists[shift+i].GetXaxis().SetTitle(vec[5])
     faccHists[shift+i].GetYaxis().SetTitle("f_{acc} lead. " + vec[5])
-    faccHists[shift+i].GetYaxis().SetMinimum(0)
+    faccHists[shift+i].GetYaxis().SetRangeUser(0.,1.)
     faccHists[shift+i].Draw("*H")
     c.SaveAs(outputFacc + "/fake_hits_rate_" + vec[1] + "1.pdf")
+    faccHists[shift+i].Write()
+  fakeFile.Close()
   c.Close()
 
 def drawHists():
@@ -269,84 +285,84 @@ partOnly = 0
 observables = [
 		["Int_t",	"nbjets", 8, 0, 8, "n_{b,jets}"],
 		["Int_t",	"njets", 15, 0, 15, "n_{jets}"],
-		["Double_t",    "etdr", 20, 0, 400e+03, "E_{T}\DeltaR"],
+		["Double_t",    "etdr", 20, 0, 400e+03, "E_{T}#Delta R"],
 #                ["Double_t",    "met", 20, 0, 1100e+03, "Missing E_{T}"],
-                ["Double_t",    "met_ex", 20, -1000e+03, 1000e+03, "MET E_{x}"],
-                ["Double_t",    "met_ey", 20, -1000e+03, 1000e+03, "MET E_{y}"],
+                ["Double_t",    "met_ex", 20, -800e+03, 800e+03, "MET E_{x}"],
+                ["Double_t",    "met_ey", 20, -800e+03, 800e+03, "MET E_{y}"],
 #                ["Double_t",    "met_phi", 20, -3.1416, 3.1416, "MET \phi"],
-                ["Double_t",    "mlb_minavg", 20, 0, 400e+03, "m_{lb,minavg}"],
-                ["Double_t",    "mlb_minavglow", 20, 0, 300e+03, "m_{lb,minavglow}"],
-                ["Double_t",    "mlb_minavghigh", 20, 0, 700e+03, "m_{lb,minavghigh}"],
-                ["Double_t",    "mlb_minmax", 20, 0, 700e+03, "m_{lb,minmax}"],
-                ["Double_t",    "mlb_minmaxlow", 20, 0, 300e+03, "m_{lb,minmaxlow}"],
+                ["Double_t",    "mlb_minavg", 20, 0, 350e+03, "m_{lb,minavg}"],
+                ["Double_t",    "mlb_minavglow", 20, 0, 200e+03, "m_{lb,minavglow}"],
+                ["Double_t",    "mlb_minavghigh", 20, 0, 400e+03, "m_{lb,minavghigh}"],
+                ["Double_t",    "mlb_minmax", 20, 0, 200e+03, "m_{lb,minmax}"],
+                ["Double_t",    "mlb_minmaxlow", 20, 0, 200e+03, "m_{lb,minmaxlow}"],
 #                ["Double_t",    "mlb_minmaxhigh", 20, 0, 300e+03, "m_{lb}"],
                 ["Double_t",    "pTlb_1", 20, 0, 600e+03, "p_{T,lb}_{1}"],
                 ["Double_t",    "pTlb_2", 20, 0, 500e+03, "p_{T,lb}_{2}"],
-                ["Double_t",    "dRlb_1", 20, 0, 6, "\DeltaR_{lb}_{1}"],
-                ["Double_t",    "dRlb_2", 20, 0, 6, "\DeltaR_{lb}_{2}"],
-                ["Double_t",    "mll", 20, 0, 900e+03, "m_{ll}"],
+                ["Double_t",    "dRlb_1", 20, 0, 4, "#Delta R_{lb}_{1}"],
+                ["Double_t",    "dRlb_2", 20, 0, 4, "#Delta R_{lb}_{2}"],
+                ["Double_t",    "mll", 20, 0, 800e+03, "m_{ll}"],
                 ["Double_t",    "pTll", 20, 0, 500e+03, "p_{T,ll}"],
-                ["Double_t",    "dRll", 20, 0, 6, "\DeltaR_{ll}"],
-                ["Double_t",    "mbb", 20, 0, 2300e+03, "m_{bb}"],
-                ["Double_t",    "pTbb", 20, 0, 900e+03, "p_{T,bb}"],
-                ["Double_t",    "dRbb", 20, 0, 6, "\DeltaR_{bb}"],
-                ["float",    "top_pt", 20, 0, 900e+03, "Top p_{T}"],
-                ["float",    "top_eta", 20, -5, 5, "Top \eta"],
-                ["float",    "top_phi", 20, -3.1416, 3.1416, "Top \phi"],
+                ["Double_t",    "dRll", 20, 0, 6, "#Delta R_{ll}"],
+                ["Double_t",    "mbb", 20, 0, 1600e+03, "m_{bb}"],
+                ["Double_t",    "pTbb", 20, 0, 800e+03, "p_{T,bb}"],
+                ["Double_t",    "dRbb", 20, 0, 6, "#Delta R_{bb}"],
+                ["float",    "top_pt", 20, 0, 800e+03, "Top p_{T}"],
+                ["float",    "top_eta", 20, -5, 5, "Top #eta"],
+                ["float",    "top_phi", 20, -3.1416, 3.1416, "Top #phi"],
                 ["float",    "top_m", 20, 0, 500e+03, "Top m"],
-                ["float",    "top_e", 20, 0, 4100e+03, "Top E"],
+                ["float",    "top_e", 20, 0, 2000e+03, "Top E"],
                 ["float",    "top_y", 20, -2.9, 2.9, "Top y"],
  #               ["float",    "top_ratio", 20, 0, 1, "m_{lb}"],
-                ["float",    "tbar_pt", 20, 0, 900e+03, "Tbar p_{T}"],
-                ["float",    "tbar_eta", 20, -5, 5, "Tbar \eta"],
-                ["float",    "tbar_phi", 20, -3.1416, 3.1416, "Tbar \phi"],
-                ["float",    "tbar_m", 20, 0, 600e+03, "Tbar m"],
-                ["float",    "tbar_e", 20, 0, 2700e+03, "Tbar E"],
-                ["float",    "tbar_y", 20, -2.9, 2.9, "Tbar y"],
+                ["float",    "tbar_pt", 20, 0, 800e+03, "tbar p_{T}"],
+                ["float",    "tbar_eta", 20, -5, 5, "tbar #eta"],
+                ["float",    "tbar_phi", 20, -3.1416, 3.1416, "tbar #phi"],
+                ["float",    "tbar_m", 20, 0, 600e+03, "tbar m"],
+                ["float",    "tbar_e", 20, 0, 2000e+03, "tbar E"],
+                ["float",    "tbar_y", 20, -2.9, 2.9, "tbar y"],
  #               ["float",    "tbar_ratio", 20, 0, 1, "m_{lb}"],
                 ["float",    "av_top_pt", 20, 0, 900e+03, "Av. top p_{T}"],
-                ["float",    "av_top_eta", 20, -5, 5, "Av. top \eta"],
-                ["float",    "av_top_phi", 20, -3.1416, 3.1416, "Av. top \phi"],
+                ["float",    "av_top_eta", 20, -5, 5, "Av. top #eta"],
+                ["float",    "av_top_phi", 20, -3.1416, 3.1416, "Av. top #phi"],
                 ["float",    "av_top_m", 20, 0, 500e+03, "Av. top m"],
                 ["float",    "av_top_e", 20, 0, 2400e+03, "Av. top E"],
                 ["float",    "av_top_y", 20, -2.9, 2.9, "Av. top y"],
-                ["float",    "ttbar_pt", 20, 0, 900e+03, "ttbar p_{T}"],
-                ["float",    "ttbar_eta", 20, -10, 10, "ttbar \eta"],
-                ["float",    "ttbar_phi", 20, -3.1416, 3.1416, "ttbar \phi"],
+                ["float",    "ttbar_pt", 20, 0, 800e+03, "ttbar p_{T}"],
+                ["float",    "ttbar_eta", 20, -10, 10, "ttbar #eta"],
+                ["float",    "ttbar_phi", 20, -3.1416, 3.1416, "ttbar #phi"],
                 ["float",    "ttbar_m", 20, 0, 2500e+03, "ttbar m"],
-                ["float",    "ttbar_e", 20, 0, 4700e+03, "ttbar E"],
+                ["float",    "ttbar_e", 20, 0, 3000e+03, "ttbar E"],
                 ["float",    "ttbar_y", 20, -2.9, 2.9, "ttbar y"], 
-                ["float",    "ttbar_pout", 20, -700e+03, 700e+03, "ttbar p_{out}"],
-                ["float",    "nu_pt", 20, 0, 600e+03, "\nu p_{T}"], 
-                ["float",    "nu_eta", 20, -10, 10, "\nu \eta"],
-                ["float",    "nu_phi", 20, -3.1416, 3.1416, "\nu \phi"],
-                ["float",    "nu_m", 20, -0.2e+03, 0.2e+03, "\nu m"],
-                ["float",    "nu_e", 20, 0, 3500e+03, "\nu E"],
-                ["float",    "nu_y", 20, -2.9, 2.9, "\nu y"],
-                ["float",    "nubar_pt", 20, 0, 600e+03, "\nubar p_{T}"],
-                ["float",    "nubar_eta", 20, -10, 10, "\nubar \eta"],
-                ["float",    "nubar_phi", 20, -3.1416, 3.1416, "\nubar \phi"],
-                ["float",    "nubar_m", 20, -0.5e+03, 0.5e+03, "\nubar m"],
-                ["float",    "nubar_e", 20, 0, 1800e+03, "\nubar E"],
-                ["float",    "nubar_y", 20, -2.9, 2.9, "\nubar y"],
-                ["float",    "Wp_pt", 20, 0, 800e+03, "W^{+} p_{T}"],
-                ["float",    "Wp_eta", 20, -10, 10, "W^{+} \eta"],
-                ["float",    "Wp_phi", 20, -3.1416, 3.1416, "W^{+} \phi"],
-                ["float",    "Wp_m", 20, 0, 1000e+03, "W^{+} m"],
-                ["float",    "Wp_e", 20, 0, 3600e+03, "W^{+} E"],
+                ["float",    "ttbar_pout", 20, -400e+03, 400e+03, "ttbar p_{out}"],
+                ["float",    "nu_pt", 20, 0, 500e+03, "#nu p_{T}"], 
+                ["float",    "nu_eta", 20, -6, 6, "#nu #eta"],
+                ["float",    "nu_phi", 20, -3.1416, 3.1416, "#nu #phi"],
+                ["float",    "nu_m", 20, -0.2e+03, 0.2e+03, "#nu m"],
+                ["float",    "nu_e", 20, 0, 1400e+03, "#nu E"],
+                ["float",    "nu_y", 20, -2.9, 2.9, "#nu y"],
+                ["float",    "nubar_pt", 20, 0, 500e+03, "#nu bar p_{T}"],
+                ["float",    "nubar_eta", 20, -5, 5, "#nu bar #eta"],
+                ["float",    "nubar_phi", 20, -3.1416, 3.1416, "#nu bar #phi"],
+                ["float",    "nubar_m", 20, -0.3e+03, 0.3e+03, "#nu bar m"],
+                ["float",    "nubar_e", 20, 0, 1400e+03, "#nu bar E"],
+                ["float",    "nubar_y", 20, -2.9, 2.9, "#nu bar y"],
+                ["float",    "Wp_pt", 20, 0, 500e+03, "W^{+} p_{T}"],
+                ["float",    "Wp_eta", 20, -6, 6, "W^{+} #eta"],
+                ["float",    "Wp_phi", 20, -3.1416, 3.1416, "W^{+} #phi"],
+                ["float",    "Wp_m", 20, 0, 400e+03, "W^{+} m"],
+                ["float",    "Wp_e", 20, 0, 1500e+03, "W^{+} E"],
                 ["float",    "Wp_y", 20, -2.9, 2.9, "W^{+} y"], 
-                ["float",    "Wm_pt", 20, 0, 800e+03, "W^{-} p_{T}"],
-                ["float",    "Wm_eta", 20, -10, 10, "W^{-} \eta"],
-                ["float",    "Wm_phi", 20, -3.1416, 3.1416, "W^{-} \phi"],
-                ["float",    "Wm_m", 20, 0, 1000e+03, "W^{-} m"],
-                ["float",    "Wm_e", 20, 0, 1900e+03, "W^{-} E"],
+                ["float",    "Wm_pt", 20, 0, 500e+03, "W^{-} p_{T}"],
+                ["float",    "Wm_eta", 20, -6, 6, "W^{-} #eta"],
+                ["float",    "Wm_phi", 20, -3.1416, 3.1416, "W^{-} #phi"],
+                ["float",    "Wm_m", 20, 0, 400e+03, "W^{-} m"],
+                ["float",    "Wm_e", 20, 0, 1500e+03, "W^{-} E"],
                 ["float",    "Wm_y", 20, -2.9, 2.9, "W^{-} y"]
 #                ["Int_t",       "nu_n", 20, 0, 100e+03, "m_{lb}"]
 		]
 
 vectors = [
-		["double",	"bjet_pt", 100, 0, 990e+03, "b_{jet} p_{T}"],
-		["double",	"el_pt", 100, 0, 990e+03, "Electron p_{T}"]
+		["double",	"bjet_pt", 20, 0, 990e+03, "b_{jet} p_{T}"],
+		["float",	"el_pt", 20, 0, 700e+03, "Electron p_{T}"]
 		]
 
 # Declare migration matrices, reco/part. histograms, eff. & fake hits
@@ -366,10 +382,10 @@ for obs in observables:
   xMin = obs[3]
   xMax = obs[4]
   obsName = obs[5]
-  migrationMatrices.append( TH2F(mig, "A_ij "+obsName, nBins, xMin, xMax, nBins, xMin, xMax ) )
+  migrationMatrices.append( TH2F(mig, "A_{ij} "+obsName, nBins, xMin, xMax, nBins, xMin, xMax ) )
   partHists.append( TH1F(histP, obsName+"^{part}", nBins, xMin, xMax ) )
   recoHists.append( TH1F(histR, obsName+"^{reco}", nBins, xMin, xMax ) )
-  effHists.append( TH1F(eff, "\epsilon_{eff} "+obsName, nBins, xMin, xMax ) )
+  effHists.append( TH1F(eff, "#epsilon_{eff} "+obsName, nBins, xMin, xMax ) )
   faccHists.append( TH1F(facc, "f_{acc} "+obsName, nBins, xMin, xMax ) )
 
 for vec in vectors:
@@ -382,10 +398,10 @@ for vec in vectors:
   xMin = vec[3]
   xMax = vec[4]
   obsName = vec[5]
-  migrationMatrices.append( TH2F(mig, "A_ij lead. "+obsName, nBins, xMin, xMax, nBins, xMin, xMax ) )
+  migrationMatrices.append( TH2F(mig, "A_{ij} lead. "+obsName, nBins, xMin, xMax, nBins, xMin, xMax ) )
   partHists.append( TH1F(histP, obsName+"1^{part}", nBins, xMin, xMax ) )
   recoHists.append( TH1F(histR, obsName+"1^{reco}", nBins, xMin, xMax ) )
-  effHists.append( TH1F(eff, "\epsilon_{eff} lead. "+obsName, nBins, xMin, xMax ) )
+  effHists.append( TH1F(eff, "#epsilon_{eff} lead. "+obsName, nBins, xMin, xMax ) )
   faccHists.append( TH1F(facc, "f_{acc} lead. "+obsName, nBins, xMin, xMax ) )
 
 shift = len(observables)
